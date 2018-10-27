b9c5d562dde9b91a4e67a62cf5b357d9c21d20ad
Juned Chhipa <juned.chhipa@gmail.com>
2018-10-27 16:25:29 +0530

    puppeteer test cases added for null data

---
+++ tests/e2e/spec/area/missing-data.spec.js
@@ -0,0 +1,26 @@
+import puppeteer from "puppeteer";
+import { root } from '../../../../config.js';
+
+const APP = root + '/samples/area/area-with-missing-data.html';
+const screenshotPath = root + '/tests/e2e/snapshots/area-with-missing-data.png'
+
+describe("Rendering Charts containing null data", () => {
+  it('should render missing values in an area chart', async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('file://' + APP);
+
+    await page.waitFor(2000);
+
+    const paths = await page.$('.apexcharts-area-series')
+  
+    const attrD = await paths.$$eval('path', nodes => nodes.map(n => n.getAttribute('d')))
+  
+    attrD.forEach((d) => {
+      expect(d).toEqual(expect.not.stringContaining('NaN'))
+    })
+
+    await browser.close();
+  })
+  
+}, 10000);
---
+++ tests/e2e/spec/line/missing-data.spec.js
@@ -0,0 +1,26 @@
+import puppeteer from "puppeteer";
+import { root } from '../../../../config.js';
+
+const APP = root + '/samples/line/line-with-missing-data.html';
+const screenshotPath = root + '/tests/e2e/snapshots/line-with-missing-data.png'
+
+describe("Rendering Charts containing null data", () => {
+  it('should render missing values in a line chart', async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('file://' + APP);
+
+    await page.waitFor(2000);
+
+    const paths = await page.$('.apexcharts-line-series')
+  
+    const attrD = await paths.$$eval('path', nodes => nodes.map(n => n.getAttribute('d')))
+  
+    attrD.forEach((d) => {
+      expect(d).toEqual(expect.not.stringContaining('NaN'))
+    })
+
+    await browser.close();
+  })
+  
+}, 10000);
