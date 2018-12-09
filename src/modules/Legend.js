d872471f5868b82de774b61bbeb2726a14652b07
Juned Chhipa <juned.chhipa@gmail.com>
2018-12-09 19:05:33 +0530

    IE11 legend fix

--- src/modules/Legend.js
+++ src/modules/Legend.js
@@ -31,7 +31,12 @@ class Legend {
       }
 
       this.drawLegends()
-      this.appendToForeignObject()
+      if (!Utils.isIE11()) {
+        this.appendToForeignObject()        
+      } else {
+        // IE11 doesn't supports foreignObject, hence append it to <head>
+        document.getElementsByTagName('head')[0].appendChild(this.getLegendStyles())
+      }
 
       if (cnf.legend.position === 'bottom' || cnf.legend.position === 'top') {
         this.legendAlignHorizontal()
