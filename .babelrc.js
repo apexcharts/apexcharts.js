75136dbf7d38366602aad287997234865909107f
Carl St-Laurent <cstlaurent@genetec.com>
2019-01-10 12:44:14 -0500

    started Babel config

--- .babelrc.js
+++ .babelrc.js
@@ -1,3 +1,13 @@
 module.exports = {
-  presets: ["env"]
+  presets: [
+    require('@babel/preset-env'),
+  ],
+  plugins: [
+    require('@babel/plugin-syntax-dynamic-import')
+  ],
+  ignore: [
+    'build/*.js',
+    'dist/*.js',
+    'samples/**/*.js'
+  ]
 }
--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,17 @@
 module.exports = {
     root: true,
+    parserOptions: {
+        ecmaVersion: 2018,
+        parser: 'babel-eslint',
+        sourceType: 'module'
+    },
     env: {
         browser: true,
+        es6: true,
         node: true
     },
     extends: ['plugin:prettier/recommended'],
-    "globals": {
+    globals: {
         "Blob": true,
         "Image": true,
         "URL": true,
@@ -13,13 +19,7 @@ module.exports = {
         "ApexCharts": true,
         "screen": true
     },
-    parserOptions: {
-        ecmaVersion: 2018,
-        parser: 'babel-eslint',
-        sourceType: 'module'
-    },
     rules: {
-
         'space-before-function-paren': 0 // Do not clash with Prettier
     }
 };
