1b4be584fb19cf9eb0cb8ab921677dbe2eba4d5c
Juned Chhipa <juned.chhipa@gmail.com>
2019-01-12 18:02:27 +0530

    replace shadeColor function with a minimal version

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -184,43 +184,22 @@ class Utils {
      ('0' + parseInt(rgb[3], 10).toString(16)).slice(-2) : ''
   }
 
-  // beautiful color shading blending code
-  // http://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color-or-rgb-and-blend-colors
-  shadeColor (p, from, to = undefined) {
-    if (from[0] !== '#' || from[0] !== 'r') {
-      from = this.getHexColorFromName(from)
-    }
-    if (typeof (p) !== 'number' || p < -1 || p > 1 || typeof (from) !== 'string' || (from[0] !== 'r' && from[0] !== '#') || (typeof (to) !== 'string' && typeof (to) !== 'undefined')) return null // ErrorCheck
-    var i = parseInt,
-      r = Math.round,
-      h = from.length > 9,
-      h = typeof (to) === 'string' ? to.length > 9 ? true : to === 'c' ? !h : false : h,
-      b = p < 0,
-      p = b ? p * -1 : p,
-      to = to && to !== 'c' ? to : b ? '#000000' : '#FFFFFF',
-      f = this.sbcRip(from),
-      t = this.sbcRip(to)
-    if (!f || !t) return null // ErrorCheck
-    if (h) return 'rgb(' + r((t[0] - f[0]) * p + f[0]) + ',' + r((t[1] - f[1]) * p + f[1]) + ',' + r((t[2] - f[2]) * p + f[2]) + (f[3] < 0 && t[3] < 0 ? ')' : ',' + (f[3] > -1 && t[3] > -1 ? r(((t[3] - f[3]) * p + f[3]) * 10000) / 10000 : t[3] < 0 ? f[3] : t[3]) + ')')
-    else return '#' + (0x100000000 + (f[3] > -1 && t[3] > -1 ? r(((t[3] - f[3]) * p + f[3]) * 255) : t[3] > -1 ? r(t[3] * 255) : f[3] > -1 ? r(f[3] * 255) : 255) * 0x1000000 + r((t[0] - f[0]) * p + f[0]) * 0x10000 + r((t[1] - f[1]) * p + f[1]) * 0x100 + r((t[2] - f[2]) * p + f[2])).toString(16).slice(f[3] > -1 || t[3] > -1 ? 1 : 3)
+  shadeRGBColor(percent, color) {
+    var f=color.split(","),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=parseInt(f[0].slice(4)),G=parseInt(f[1]),B=parseInt(f[2]);
+    return "rgb("+(Math.round((t-R)*p)+R)+","+(Math.round((t-G)*p)+G)+","+(Math.round((t-B)*p)+B)+")";
   }
 
-  // helper function of shadeColor
-  sbcRip (d) {
-    var l = d.length,
-      RGB = new Object()
-    var i = parseInt,
-      r = Math.round
-    if (l > 9) {
-      d = d.split(',')
-      if (d.length < 3 || d.length > 4) return null // ErrorCheck
-      RGB[0] = i(d[0].slice(4)), RGB[1] = i(d[1]), RGB[2] = i(d[2]), RGB[3] = d[3] ? parseFloat(d[3]) : -1
-    } else {
-      if (l === 8 || l === 6 || l < 4) return null // ErrorCheck
-      if (l < 6) d = '#' + d[1] + d[1] + d[2] + d[2] + d[3] + d[3] + (l > 4 ? d[4] + '' + d[4] : '') // 3 digit
-      d = i(d.slice(1), 16), RGB[0] = d >> 16 & 255, RGB[1] = d >> 8 & 255, RGB[2] = d & 255, RGB[3] = l === 9 || l === 5 ? r(((d >> 24 & 255) / 255) * 10000) / 10000 : -1
-    }
-    return RGB
+  shadeHexColor(percent, color) {
+    var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
+    return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
+  }
+
+  // beautiful color shading blending code
+  // http://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color-or-rgb-and-blend-colors
+  shadeColor (p, color) {
+
+    if (color.length > 7 ) return this.shadeRGBColor(p, color);
+    else return this.shadeHexColor(p, color);
   }
 
   // https://stackoverflow.com/questions/1573053/javascript-function-to-convert-color-names-to-hex-codes
