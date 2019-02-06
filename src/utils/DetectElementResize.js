b5c526c8a51f8ef1160e028bc7a4ff49a1acebdd
Juned Chhipa <juned.chhipa@gmail.com>
2019-02-07 01:08:20 +0530

    check el existance before removing listener - fixes apexcharts/vue-apexcharts#54

--- src/utils/DetectElementResize.js
+++ src/utils/DetectElementResize.js
@@ -127,11 +127,12 @@
   }
 
   window.removeResizeListener = function (element, fn) {
-    element.__resizeListeners__.splice(element.__resizeListeners__.indexOf(fn), 1)
-    if (!element.__resizeListeners__.length) {
-      element.removeEventListener('scroll', scrollListener)
-      element.__resizeTriggers__ = !element.removeChild(element.__resizeTriggers__)        
-    }
-    
+    if (element) {
+      element.__resizeListeners__.splice(element.__resizeListeners__.indexOf(fn), 1)  
+      if (!element.__resizeListeners__.length) {
+        element.removeEventListener('scroll', scrollListener)
+        element.__resizeTriggers__ = !element.removeChild(element.__resizeTriggers__)        
+      }
+    }    
   }
 })()
