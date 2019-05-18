074beb64f934210febd34313ba93d82939157721
Juned Chhipa <juned.chhipa@gmail.com>
2019-05-19 04:49:18 +0530

    prevent overflow of markers

--- src/modules/axes/Grid.js
+++ src/modules/axes/Grid.js
@@ -133,16 +133,13 @@ class Grid {
     const coreUtils = new CoreUtils(this)
     coreUtils.getLargestMarkerSize()
 
-    let markerSize =
-      w.globals.markers.largestSize + w.config.markers.hover.sizeOffset + 1
-
-    if (markerSize < 10) markerSize = 10
+    let markerSize = w.globals.markers.largestSize + 1
 
     gl.dom.elGridRectMarker = graphics.drawRect(
-      -markerSize * 2,
-      -markerSize * 2,
-      gl.gridWidth + markerSize * 4,
-      gl.gridHeight + markerSize * 4,
+      -markerSize,
+      -markerSize,
+      gl.gridWidth + markerSize * 2,
+      gl.gridHeight + markerSize * 2,
       0,
       '#fff'
     )
