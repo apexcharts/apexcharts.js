d8a26759a4a777bdaff221e1f221449cad76ea6c
Juned Chhipa <juned.chhipa@gmail.com>
2019-06-23 01:50:51 +0530

    add a decimal point to small values to prevent duplicate yaxis labels

--- src/modules/Formatters.js
+++ src/modules/Formatters.js
@@ -114,7 +114,7 @@ class Formatters {
                   ? yaxe.decimalsInFloat
                   : w.globals.yValueDecimal
               )
-            } else if (w.globals.maxYArr[i] - w.globals.minYArr[i] < 5) {
+            } else if (w.globals.maxYArr[i] - w.globals.minYArr[i] < 10) {
               return val.toFixed(1)
             } else {
               return val.toFixed(0)
