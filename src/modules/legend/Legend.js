556d9cd75be468ba20e67fea152d40e5a3eda1e1
Juned Chhipa <juned.chhipa@gmail.com>
2020-01-12 12:24:31 +0530

    legend margin fix - fixes #1203

--- src/modules/legend/Legend.js
+++ src/modules/legend/Legend.js
@@ -239,7 +239,7 @@ class Legend {
       )
 
       elLegend.classList.add('apexcharts-legend-series')
-      elLegend.style.margin = `${w.config.legend.itemMargin.horizontal}px ${w.config.legend.itemMargin.vertical}px`
+      elLegend.style.margin = `${w.config.legend.itemMargin.vertical}px ${w.config.legend.itemMargin.horizontal}px`
       w.globals.dom.elLegendWrap.style.width = w.config.legend.width
         ? w.config.legend.width + 'px'
         : ''
