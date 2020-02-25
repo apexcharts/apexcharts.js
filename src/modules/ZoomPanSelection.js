6e6051406befff62a8ff050155c90841b4845273
Tian Xie <xie.tim.tian@gmail.com>
2020-02-25 16:08:01 -0500

    fix issue#1293: reset xaxis fails on multiple charts

--- src/modules/ZoomPanSelection.js
+++ src/modules/ZoomPanSelection.js
@@ -505,6 +505,8 @@ export default class ZoomPanSelection extends Toolbar {
       if (w.globals.zoomEnabled) {
         let yaxis = Utils.clone(w.globals.initialConfig.yaxis)
 
+        w.globals.zoomed = true // set zoomed to true to prevent the block below running
+
         // before zooming in/out, store the last yaxis and xaxis range, so that when user hits the RESET button, we get the original range
         // also - make sure user is not already zoomed in/out - otherwise we will store zoomed values in lastAxis
         if (!w.globals.zoomed) {
@@ -571,7 +573,8 @@ export default class ZoomPanSelection extends Toolbar {
           toolbar.zoomCallback(xaxis, yaxis)
         }
 
-        w.globals.zoomed = true
+        // move to line 511
+        // w.globals.zoomed = true
       } else if (w.globals.selectionEnabled) {
         let yaxis = null
         let xaxis = null
