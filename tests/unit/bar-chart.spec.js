8041c20b5706b0e7e76a2a4a7a4304c189773fb7
Maxime Goyette <maxime.goyette@flare.systems>
2021-01-05 12:44:14 -0500

    minXDiff must be computed using the sorted array

---
+++ tests/unit/bar-chart.spec.js
@@ -0,0 +1,22 @@
+import Range from '../../src/modules/Range.js'
+import { createChartWithOptions } from './utils/utils.js'
+
+describe('Bar chart', () => {
+  it('columns should not overlap because of wrong minXDiff value', () => {
+    const chart = createChartWithOptions({
+      series: [
+        {
+          data: [[1, 1], [4, 4], [3, 3]]
+        }
+      ],
+      chart: {
+        type: 'bar',
+      },
+    })
+
+    const range = new Range(chart)
+    range.setXRange()
+
+    expect(range.w.globals.minXDiff).toEqual(1)
+  })
+})
