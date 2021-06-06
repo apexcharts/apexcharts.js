c7be44c3bc1bd2b34b4e42e962852deb0e1e2196
junedchhipa <juned.chhipa@gmail.com>
2021-06-06 19:05:37 +0530

    fix legend type

--- types/apexcharts.d.ts
+++ types/apexcharts.d.ts
@@ -741,7 +741,7 @@ type ApexLegend = {
   formatter?(legendName: string, opts?: any): string
   tooltipHoverFormatter?(legendName: string, opts?: any): string
   textAnchor?: string
-  customLegendItems: string[]
+  customLegendItems?: string[]
   labels?: {
     colors?: string | string[]
     useSeriesColors?: boolean
