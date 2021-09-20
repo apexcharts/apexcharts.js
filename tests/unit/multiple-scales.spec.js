678edae56f18909003cae8413e3f0dc143affdbb
Gal Sisso <gal.sisso@intel.com>
2021-09-20 12:09:20 +0300

    Fix tests

--- tests/unit/multiple-scales.spec.js
+++ tests/unit/multiple-scales.spec.js
@@ -3,7 +3,7 @@ import { createChartWithOptions } from './utils/utils.js'
 import { logData } from './data/log-data.js'
 
 describe('Multiple Y-axis Scales', () => {
-  it('should return correct scales for log and linear yaxis scales', () => {
+  it('should return correct scales for log and linear yaxis scales when no logarithmic base specified', () => {
     const chart = createChartWithOptions({
       chart: {
         type: 'line'
@@ -22,7 +22,9 @@ describe('Multiple Y-axis Scales', () => {
         {
           max: 500000000,
           tickAmount: 4,
-          logarithmic: true,
+          logarithmic: {
+            isActive: true
+          },
           seriesName: 'Logarithmic'
         },
         {
@@ -59,6 +61,63 @@ describe('Multiple Y-axis Scales', () => {
           100000000,
           1000000000
         ]
+      },
+      {
+        niceMax: 500000000,
+        niceMin: 1000000,
+        result: [1000000, 125750000, 250500000, 375250000, 500000000]
+      }
+    ])
+  })
+
+  it('should return correct scales for log and linear yaxis scales when logarithmic base is 20', () => {
+    const chart = createChartWithOptions({
+      chart: {
+        type: 'line'
+      },
+      series: [
+        {
+          name: 'Logarithmic',
+          data: logData
+        },
+        {
+          name: 'Linear',
+          data: logData
+        }
+      ],
+      yaxis: [
+        {
+          max: 500000000,
+          tickAmount: 4,
+          logarithmic: {
+            isActive: true,
+            base: 20
+          },
+          seriesName: 'Logarithmic'
+        },
+        {
+          min: 1000000,
+          max: 500000000,
+          opposite: true,
+          tickAmount: 4,
+          seriesName: 'Linear'
+        }
+      ]
+    })
+
+    const minYArr = chart.w.globals.minYArr
+    const maxYArr = chart.w.globals.maxYArr
+    const yAxisScale = chart.w.globals.yAxisScale
+
+    expect(minYArr).toEqual([1, 1000000])
+
+    expect(maxYArr).toEqual([1280000000, 500000000])
+
+    expect(yAxisScale).toEqual([
+      {
+        niceMax: 1280000000,
+        niceMin: 1,
+        result: [1, 20, 400, 8000, 160000, 3200000, 64000000, 1280000000]
       },
       {
         niceMax: 500000000,
