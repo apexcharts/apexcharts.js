6d4ca0509986578cedc6b0b062b550110bff64eb
Ruben Stolk <ruben.stolk@capptions.com>
2021-12-15 10:03:30 +0100

    Fix for not rendering 0 as min when using logarithmic
    Should fix https://github.com/apexcharts/apexcharts.js/issues/2673

--- src/modules/CoreUtils.js
+++ src/modules/CoreUtils.js
@@ -304,11 +304,13 @@ class CoreUtils {
 
   getLogVal(d, yIndex) {
     const w = this.w
-    return (
+    const lv = (
       (Math.log(d) - Math.log(w.globals.minYArr[yIndex])) /
       (Math.log(w.globals.maxYArr[yIndex]) -
         Math.log(w.globals.minYArr[yIndex]))
     )
+
+    return isNaN(lv) ? d : lv
   }
 
   getLogYRatios(yRatio) {
