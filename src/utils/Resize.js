cf0d2cf98fe544ced23d4cb4a03fc3294cccd8a7
junedchhipa <juned.chhipa@gmail.com>
2022-02-20 20:19:17 +0530

    add a check in resize observer if el is hidden initially - fixes #2825, fixes #2991, fixes #2992

--- src/utils/Resize.js
+++ src/utils/Resize.js
@@ -6,6 +6,16 @@ let ros = new WeakMap() // Map callbacks to ResizeObserver instances for easy re
 export function addResizeListener(el, fn) {
   let called = false
 
+  const elRect = el.getBoundingClientRect()
+  if (el.style.display === 'none' || elRect.width === 0) {
+    // if elRect.width=0, the chart is not rendered at all
+    // (it has either display none or hidden in a different tab)
+    // fixes https://github.com/apexcharts/apexcharts.js/issues/2825
+    // fixes https://github.com/apexcharts/apexcharts.js/issues/2991
+    // fixes https://github.com/apexcharts/apexcharts.js/issues/2992
+    called = true
+  }
+
   let ro = new ResizeObserver((r) => {
     // ROs fire immediately after being created,
     // per spec: https://drafts.csswg.org/resize-observer/#ref-for-element%E2%91%A3
