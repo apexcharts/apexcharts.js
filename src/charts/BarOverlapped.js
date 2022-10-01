859ffad013c2770f477232c655b4a273d617658e
Marvin <marvin@fretly.fr>
2022-10-01 13:53:14 +0200

    feat: align labels

--- src/charts/BarOverlapped.js
+++ src/charts/BarOverlapped.js
@@ -179,6 +179,8 @@ class BarOverlapped extends BarStacked {
       yArrValues,
       this.series,
       elDataLabelsWrap,
+      zeroW,
+      yDivision,
       nextValue
     )
 
@@ -195,124 +197,6 @@ class BarOverlapped extends BarStacked {
 
       this.reorderedValues[j] = orderedValues
     }
-  }
-
-  getElSeries(i, bc, x, y, series, seriesIndex) {
-    const w = this.w
-    let xDivision // xDivision is the GRIDWIDTH divided by number of datapoints (columns)
-    let yDivision // yDivision is the GRIDHEIGHT divided by number of datapoints (bars)
-    let zeroH // zeroH is the baseline where 0 meets y axis
-    let zeroW // zeroW is the baseline where 0 meets x axis
-
-    let xArrValues = []
-    let yArrValues = []
-
-    let realIndex = w.globals.comboCharts ? seriesIndex[i] : i
-
-    if (this.yRatio.length > 1) {
-      this.yaxisIndex = realIndex
-    }
-
-    this.isReversed =
-      w.config.yaxis[this.yaxisIndex] &&
-      w.config.yaxis[this.yaxisIndex].reversed
-
-    // el to which series will be drawn
-    let elSeries = this.graphics.group({
-      class: `apexcharts-series`,
-      seriesName: Utils.escapeString(w.globals.seriesNames[realIndex]),
-      rel: i + 1,
-      'data:realIndex': realIndex
-    })
-    this.ctx.series.addCollapsedClassToSeries(elSeries, realIndex)
-
-    // eldatalabels
-    let elDataLabelsWrap = this.graphics.group({
-      class: 'apexcharts-datalabels',
-      'data:realIndex': realIndex
-    })
-
-    let elGoalsMarkers = this.graphics.group({
-      class: 'apexcharts-bar-goals-markers',
-      style: `pointer-events: none`
-    })
-
-    let barHeight = 0
-    let barWidth = 0
-
-    let initPositions = this.initialPositions(
-      x,
-      y,
-      xDivision,
-      yDivision,
-      zeroH,
-      zeroW
-    )
-    y = initPositions.y
-    barHeight = initPositions.barHeight
-    yDivision = initPositions.yDivision
-    zeroW = initPositions.zeroW
-
-    x = initPositions.x
-    barWidth = initPositions.barWidth
-    xDivision = initPositions.xDivision
-    zeroH = initPositions.zeroH
-
-    this.yArrj = []
-    this.yArrjF = []
-    this.yArrjVal = []
-    this.xArrj = []
-    this.xArrjF = []
-    this.xArrjVal = []
-
-    // if (!this.horizontal) {
-    // this.xArrj.push(x + barWidth / 2)
-    // }
-
-    // fix issue #1215;
-    // where all stack bar disappear after collapsing the first series
-    // sol: if only 1 arr in this.prevY(this.prevY.length === 1) and all are NaN
-    if (this.prevY.length === 1 && this.prevY[0].every((val) => isNaN(val))) {
-      // make this.prevY[0] all zeroH
-      this.prevY[0] = this.prevY[0].map((val) => zeroH)
-      // make this.prevYF[0] all 0
-      this.prevYF[0] = this.prevYF[0].map((val) => 0)
-    }
-
-    for (let j = 0; j < w.globals.dataPoints; j++) {
-      elSeries = this.getRenderSeries(
-        i,
-        j,
-        realIndex,
-        bc,
-        x,
-        y,
-        elSeries,
-        xDivision,
-        barWidth,
-        zeroH,
-        barHeight,
-        elGoalsMarkers,
-        xArrValues,
-        yArrValues,
-        series,
-        elDataLabelsWrap
-      )
-    }
-
-    // push all x val arrays into main xArr
-    w.globals.seriesXvalues[realIndex] = xArrValues
-    w.globals.seriesYvalues[realIndex] = yArrValues
-
-    // push all current y values array to main PrevY Array
-    this.prevY.push(this.yArrj)
-    this.prevYF.push(this.yArrjF)
-    this.prevYVal.push(this.yArrjVal)
-    this.prevX.push(this.xArrj)
-    this.prevXF.push(this.xArrjF)
-    this.prevXVal.push(this.xArrjVal)
-
-    return elSeries
   }
 
   getRenderSeries(
@@ -332,6 +216,8 @@ class BarOverlapped extends BarStacked {
     yArrValues,
     series,
     elDataLabelsWrap,
+    zeroW,
+    yDivision,
     nextValue
   ) {
     const strokeWidth = this.barHelpers.getStrokeWidth(i, j, realIndex)
@@ -383,9 +269,13 @@ class BarOverlapped extends BarStacked {
     let pathFill = this.barHelpers.getPathFillColor(series, i, j, realIndex)
 
     if (nextValue) {
-      const nextBarHeight = nextValue / this.yRatio[this.yaxisIndex]
-      barHeight -= nextBarHeight
-      console.log({ nextBarHeight })
+      if (this.isHorizontal) {
+        const nextBarWidth = nextValue / this.invertedYRatio
+        barWidth -= nextBarWidth
+      } else {
+        const nextBarHeight = nextValue / this.yRatio[this.yaxisIndex]
+        barHeight -= nextBarHeight
+      }
     }
 
     return this.renderSeries({
