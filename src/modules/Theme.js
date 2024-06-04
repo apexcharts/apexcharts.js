11932defdd503fdce9e6123a4daa3599af537a2c
Sebastian-Webster <84299475+Sebastian-Webster@users.noreply.github.com>
2024-06-05 02:40:09 +1200

    fix: fixed theme change from dark mode to light mode not working

--- src/modules/Theme.js
+++ src/modules/Theme.js
@@ -178,20 +178,26 @@ export default class Theme {
   updateThemeOptions(options) {
     options.chart = options.chart || {}
     options.tooltip = options.tooltip || {}
-    const mode = options.theme.mode || 'light'
-    const palette = options.theme.palette
-      ? options.theme.palette
-      : mode === 'dark'
+    const mode = options.theme.mode
+    const background = mode === 'dark'
+      ? '#424242'
+      : mode === 'light'
+        ? '#fff'
+        : options.chart.background || '#fff'
+    const palette = mode === 'dark'
       ? 'palette4'
-      : 'palette1'
-    const foreColor = options.chart.foreColor
-      ? options.chart.foreColor
-      : mode === 'dark'
+      : mode === 'light'
+        ? 'palette1'
+        : options.theme.palette || 'palette1'
+    const foreColor = mode === 'dark'
       ? '#f6f7f8'
-      : '#373d3f'
+      : mode === 'light'
+        ? '#373d3f'
+        : options.chart.foreColor || '#373d3f'
 
     options.tooltip.theme = mode
     options.chart.foreColor = foreColor
+    options.chart.background = background
     options.theme.palette = palette
 
     return options
