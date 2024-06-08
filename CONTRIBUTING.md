b98e85bb88f0e8eafbde942acc6574332f11bc13
Sebastian-Webster <84299475+Sebastian-Webster@users.noreply.github.com>
2024-06-09 00:11:30 +1200

    perf: improved e2e test performance by up to 3x

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -110,7 +110,7 @@ npm run test
 If this command ends with an error `Error: Unable to launch browser, error message: Chromium revision is not downloaded.` then calling puppeteer install may solve the problem:
 
 ```bash
-node node_modules/puppeteer/install.js
+node node_modules/puppeteer/install.mjs
 ```
 
 E2e tests will likely fail due to minor differences in OS and the browser version used to take screenshots. To address this, before working on a feature, recapture screenshots using this command:
--- tests/e2e/spec/utils.js
+++ tests/e2e/spec/utils.js
@@ -3,6 +3,7 @@ import path from 'path'
 import pixelmatch from 'pixelmatch'
 import { PNG } from 'pngjs'
 import puppeteer from 'puppeteer'
+import { setTimeout as promisifiedTimeout } from 'timers/promises'
 
 export function chartVisualTest(type, filename, imageFilename, validate) {
   const rootDir = path.join(path.resolve(__dirname), '..', '..', '..')
@@ -18,7 +19,7 @@ export function chartVisualTest(type, filename, imageFilename, validate) {
         await page.goto(
           `file://${rootDir}/samples/vanilla-js/${type}/${filename}.html`
         )
-        await page.waitFor(2000)
+        await promisifiedTimeout(2000)
 
         await validate(page)
 
