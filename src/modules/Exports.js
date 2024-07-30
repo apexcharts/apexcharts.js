170fd6e9722eb2d970a7543f4ac8acbe83bb46c7
Rinu Saha <rs@thesocialcollective.co>
2024-07-30 16:19:17 +0530

    #4592 Toolbar PNG export with desired scale or width

--- src/modules/Exports.js
+++ src/modules/Exports.js
@@ -171,7 +171,10 @@ class Exports {
   }
 
   exportToPng() {
-    this.dataURI().then(({ imgURI, blob }) => {
+    const scale = this.w.config.chart.toolbar.export.png.scale
+    const width = this.w.config.chart.toolbar.export.png.width
+    const options = scale ? {scale: scale}: width? {width: width}: undefined
+    this.dataURI(options).then(({ imgURI, blob }) => {
       if (blob) {
         navigator.msSaveOrOpenBlob(blob, this.w.globals.chartID + '.png')
       } else {
