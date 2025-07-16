3f00f1931ef8e6e902718040f3e7fe95333aa6e4
Juned Chhipa <juned.chhipa@gmail.com>
2025-07-16 21:26:37 +0530

    convert all hex colors to uppercase for uniformity

--- src/apexcharts.js
+++ src/apexcharts.js
@@ -655,10 +655,10 @@ export default class ApexCharts {
   static getThemePalettes() {
     return {
       palette1: ['#008FFB', '#00E396', '#FEB019', '#FF4560', '#775DD0'],
-      palette2: ['#3f51b5', '#03a9f4', '#4caf50', '#f9ce1d', '#FF9800'],
-      palette3: ['#33b2df', '#546E7A', '#d4526e', '#13d8aa', '#A5978B'],
-      palette4: ['#4ecdc4', '#c7f464', '#81D4FA', '#fd6a6a', '#546E7A'],
-      palette5: ['#2b908f', '#f9a3a4', '#90ee7e', '#fa4443', '#69d2e7'],
+      palette2: ['#3F51B5', '#03A9F4', '#4CAF50', '#F9CE1D', '#FF9800'],
+      palette3: ['#33B2DF', '#546E7A', '#D4526E', '#13D8AA', '#A5978B'],
+      palette4: ['#4ECDC4', '#C7F464', '#81D4FA', '#FD6A6A', '#546E7A'],
+      palette5: ['#2B908F', '#F9A3A4', '#90EE7E', '#FA4443', '#69D2E7'],
       palette6: ['#449DD1', '#F86624', '#EA3546', '#662E9B', '#C5D86D'],
       palette7: ['#D7263D', '#1B998B', '#2E294E', '#F46036', '#E2C044'],
       palette8: ['#662E9B', '#F86624', '#F9C80E', '#EA3546', '#43BCCD'],
