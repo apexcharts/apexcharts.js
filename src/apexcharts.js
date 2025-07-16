18219902c6b984b422e733da9e1494168fea46a3
Juned Chhipa <juned.chhipa@gmail.com>
2025-07-16 21:17:36 +0530

    create a static function to get theme palettes

--- src/apexcharts.js
+++ src/apexcharts.js
@@ -652,6 +652,21 @@ export default class ApexCharts {
     return Utils.extend(target, source)
   }
 
+  static getThemePalettes() {
+    return {
+      palette1: ['#008FFB', '#00E396', '#FEB019', '#FF4560', '#775DD0'],
+      palette2: ['#3f51b5', '#03a9f4', '#4caf50', '#f9ce1d', '#FF9800'],
+      palette3: ['#33b2df', '#546E7A', '#d4526e', '#13d8aa', '#A5978B'],
+      palette4: ['#4ecdc4', '#c7f464', '#81D4FA', '#fd6a6a', '#546E7A'],
+      palette5: ['#2b908f', '#f9a3a4', '#90ee7e', '#fa4443', '#69d2e7'],
+      palette6: ['#449DD1', '#F86624', '#EA3546', '#662E9B', '#C5D86D'],
+      palette7: ['#D7263D', '#1B998B', '#2E294E', '#F46036', '#E2C044'],
+      palette8: ['#662E9B', '#F86624', '#F9C80E', '#EA3546', '#43BCCD'],
+      palette9: ['#5C4742', '#A5978B', '#8D5B4C', '#5A2A27', '#C4BBAF'],
+      palette10: ['#A300D6', '#7D02EB', '#5653FE', '#2983FF', '#00B1F2'],
+    }
+  }
+
   toggleSeries(seriesName) {
     return this.series.toggleSeries(seriesName)
   }
